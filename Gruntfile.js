0c567c5e02c7dea911402eb64fff154ea0e8ac0a
R.G. Wood <ric@grislyeye.com>
2021-07-02 21:54:34 +0100

    Add: standalone editable character sheet page

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,11 +27,21 @@ module.exports = function (grunt) {
       }
     },
 
+    copy: {
+      main: {
+        files: [
+          {expand: true, src: ['.nojekyll', 'index.html', 'main.css'], dest: 'dist/html', filter: 'isFile'},
+          {expand: true, src: ['node_modules/vellum-sheet/**'], dest: 'dist/html/'},
+        ],
+      },
+    },
+
   })
 
   grunt.loadNpmTasks('grunt-contrib-watch')
   grunt.loadNpmTasks('grunt-contrib-clean')
   grunt.loadNpmTasks('grunt-contrib-connect')
+  grunt.loadNpmTasks('grunt-contrib-copy')
 
   grunt.registerTask('start', ['connect', 'watch'])
 }
