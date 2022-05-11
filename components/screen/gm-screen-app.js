562441f5244fd8e35682a93825b2ea0106265587
R.G. Wood <ric@grislyeye.com>
2022-05-11 09:11:02 +0100

    Fix: screen tab headers unclear

--- components/screen/gm-screen-app.js
+++ components/screen/gm-screen-app.js
@@ -6,7 +6,6 @@ import '@kor-ui/kor/components/app-bar';
 import '@kor-ui/kor/components/card';
 import '@kor-ui/kor/components/page';
 import '@kor-ui/kor/components/tabs';
-import '@kor-ui/kor/components/text';
 import "./gm-screen-sheet-view.js"
 
 class GmScreenApp extends LitElement {
@@ -55,7 +54,6 @@ class GmScreenApp extends LitElement {
         this.sheets =
           stats
             .docs
-            .map((doc) => doc.id)
       })
     })
   }
@@ -93,10 +91,13 @@ class GmScreenApp extends LitElement {
   }
 
   renderTabs() {
+    const shorten = (str, n) => (str && str.length > n) ? str.substr(0, n-1) + '...' : str;
+
     return this.sheets.map(sheet => {
+      const name = sheet.data()['vellum-sheet-field-titlecharacter-name']
+
       return html`
-        <kor-tab-item label="${sheet}" @click="${this.changeSheet}">
-          <kor-text>${sheet}</kor-text>
+        <kor-tab-item id="${sheet.id}" label="${shorten(name, 15)}" @click="${this.changeSheet}">
         </kor-tab-item>
       `
     })
@@ -105,13 +106,13 @@ class GmScreenApp extends LitElement {
   renderSheets() {
     return this.sheets.map(sheet => {
       return html`
-        <gm-screen-sheet-view class=${this.sheet === sheet ? '' : 'hidden'} email="${sheet}"></gm-screen-sheet-view>
+        <gm-screen-sheet-view class=${this.sheet === sheet.id ? '' : 'hidden'} email="${sheet.id}"></gm-screen-sheet-view>
       `
     })
   }
 
   changeSheet(event) {
-    this.sheet = event.target.label
+    this.sheet = event.target.id
   }
 
 }
